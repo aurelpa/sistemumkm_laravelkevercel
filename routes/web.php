63640a569cf0d67ca08104e31d2cf15bd8c0fa40
aurelpa <arelggsavage780@gmail.com>
2026-06-18 21:04:43 +0700

    test root route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UmkmController;
 use App\Http\Controllers\DashboardController;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', function () {
+    return 'Laravel Berjalan';
+});
+
 Route::resource('umkm', UmkmController::class);
 
 Route::get('/debug-table', function () {
