55afb063ae3c2e55c6aba32175900e57119066b6
aurelpa <arelggsavage780@gmail.com>
2026-06-18 22:12:18 +0700

    tes api

--- api/index.php
+++ api/index.php
@@ -4,20 +4,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
 try {
-    $request = Illuminate\Http\Request::capture();
-
-    $response = $kernel->handle($request);
-
-    echo "HANDLE OK";
+    $view = app('view');
+    echo get_class($view);
 } catch (Throwable $e) {
     echo "<pre>";
-    echo "ERROR:\n\n";
     echo $e->getMessage();
-    echo "\n\nFILE: " . $e->getFile();
-    echo "\nLINE: " . $e->getLine();
-    echo "\n\nTRACE:\n";
-    echo $e->getTraceAsString();
 }
