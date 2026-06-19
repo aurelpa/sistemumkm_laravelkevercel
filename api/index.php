6c2c1d48c9466d562f792205c90d54f1ba7b6546
aurelpa <arelggsavage780@gmail.com>
2026-06-19 08:13:00 +0700

    test api index

--- api/index.php
+++ api/index.php
@@ -5,9 +5,25 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = require __DIR__ . '/../bootstrap/app.php';
 
 try {
-    $view = app('view');
-    echo get_class($view);
-} catch (Throwable $e) {
     echo "<pre>";
+
+    echo "Laravel: ".app()->version().PHP_EOL;
+
+    echo "View binding: ";
+    var_dump($app->bound('view'));
+
+    echo PHP_EOL;
+
+    echo "Bindings containing view:".PHP_EOL;
+
+    $bindings = array_keys($app->getBindings());
+
+    foreach ($bindings as $binding) {
+        if (str_contains($binding, 'view')) {
+            echo $binding.PHP_EOL;
+        }
+    }
+
+} catch (Throwable $e) {
     echo $e->getMessage();
 }
